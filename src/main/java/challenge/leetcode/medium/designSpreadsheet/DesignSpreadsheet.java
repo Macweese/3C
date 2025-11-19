c4abca4fefbee5f560896b3b726780504b8bbdd9
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 15:09:43 +0100

    solutions: cleanup lc 3484

--- src/main/java/challenge/leetcode/medium/designSpreadsheet/DesignSpreadsheet.java
+++ src/main/java/challenge/leetcode/medium/designSpreadsheet/DesignSpreadsheet.java
@@ -22,55 +22,9 @@ package challenge.leetcode.medium.designSpreadsheet;
  * @author Administrator
  * @Created 19/09/2025, 11:33 Fri 19 September 2025
  * @Project 3C: Competitive Coding Challenges
- * @Problem
- * @Name
- * @Difficulty
- * @Tags
- * @link <a href="">www.leetcode.com/999999999999999999999</a>
- * @Description Description
- *
- * <ul>
- * 		<li><b>Example 1:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 				<br>&emsp;
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 2:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 3:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- *
- * <p>
- * <b>Constraints:</b>
- * <ul>
- * 		<li></li>
- * </ul>
- * </p>
- *
- * <br>
- * <hr>
- * @Similar <a href=""/>1000. NAME (Leetcode)</a><br>
- * <br>
- * {@link solution. 10000. NAME (local)}<br>
  **/
 public class DesignSpreadsheet
 {
-	public static void main(String[] args)
-	{
-
-	}
 }
 
 class Spreadsheet
