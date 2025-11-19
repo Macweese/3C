adb45edf3587b6e0d6d78e259f04cd2fa20145a5
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 11:52:55 +0100

    solutions: cleanup lc 412

--- src/main/java/challenge/leetcode/easy/fizzBuzz/FizzBuzz.java
+++ src/main/java/challenge/leetcode/easy/fizzBuzz/FizzBuzz.java
@@ -67,14 +67,6 @@ import java.util.List;
  **/
 public class FizzBuzz
 {
-	public static void main(String[] args)
-	{
-
-		System.out.println(fizzBuzz(3));
-		System.out.println(fizzBuzz(5));
-		System.out.println(fizzBuzz(15));
-	}
-
 	public static List<String> fizzBuzz(int n)
 	{
 
