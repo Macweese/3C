c8ad25c6142b61d6c9dea358e0574900fdd0e853
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 10:43:58 +0100

    solutions: cleanup lc 976

--- src/test/java/challenge/leetcode/easy/largestPerimeterTriangle/LargestPerimeterTriangleTest.java
+++ src/test/java/challenge/leetcode/easy/largestPerimeterTriangle/LargestPerimeterTriangleTest.java
@@ -18,48 +18,36 @@
 
 package challenge.leetcode.easy.largestPerimeterTriangle;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 @DisplayName("976. Largest Perimeter Triangle")
 class LargestPerimeterTriangleTest
 {
-	@Test
-	void largestPerimeter()
+	@RepeatedTest(value = 100)
+	void largestPerimeterTest()
 	{
-		for (int[] TEST_CASE : TEST_CASES)
-		{
-			final int expected = largestPerimeter(TEST_CASE);
-			final int actual = LargestPerimeterTriangle.largestPerimeter(TEST_CASE);
+		final int[] input = generateTest();
+		final int expected = validate(input);
+		final int actual = LargestPerimeterTriangle.largestPerimeter(input);
 
-			String message = String.format("Test failed for case:"
-					+ "\n%10s : %s"
-					+ "\n%10s : %s"
-					+ "\n%10s : %s"
-					+ "\n%10s",
-				"Input", Arrays.toString(TEST_CASE),
-				"Expected", expected,
-				"Actual", actual,
-				"");
+		String message = String.format("Test failed for case:"
+				+ "\n%10s : %s"
+				+ "\n%10s : %s"
+				+ "\n%10s : %s"
+				+ "\n%10s",
+			"Input", Arrays.toString(input),
+			"Expected", expected,
+			"Actual", actual,
+			"");
 
-			assertEquals(expected, actual, message);
-		}
+		assertEquals(expected, actual, message);
 	}
 
-	static void main()
-	{
-		generateFailTest();
-		for (int i = 0; i < 50; i++)
-		{
-			generateTest();
-		}
-	}
-
-	private static void generateTest()
+	private static int[] generateTest()
 	{
 		Random r = new Random();
 		int n = r.nextInt(9_997) + 3;
@@ -70,32 +58,10 @@ class LargestPerimeterTriangleTest
 			sides[i] = r.nextInt(100_000) + 1;
 		}
 
-		TEST_CASES.add(sides);
-
-		System.out.println(Arrays.toString(sides));
-//		String input = Arrays.toString(sides).replaceAll("[\\[\\] ]", "");
-//		System.out.printf("new int[]{%s}%n", input);
+		return sides;
 	}
 
-	private static void generateFailTest()
-	{
-		Random r = new Random();
-		int n = r.nextInt(9997) + 3;
-		int[] sides = new int[n];
-
-		for (int i = 0; i < n; i++)
-		{
-//			sides[i] = FIB[r.nextInt(FIB.length)];
-			sides[i] = FIB[r.nextInt(1_000_000) + 1];
-		}
-		System.out.println(Arrays.toString(sides));
-	}
-
-	private static final int[] FIB = new int[]{1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025};
-
-	private static final ArrayList<int[]> TEST_CASES = new ArrayList<>();
-
-	private static int largestPerimeter(int[] nums)
+	private static int validate(int[] nums)
 	{
 		Arrays.sort(nums);
 
