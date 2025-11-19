4ead0e71e10a553ef992418fd16a38c2acc43ce6
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 15:26:59 +0100

    solutions: cleanup lc 695

--- src/main/java/challenge/leetcode/medium/maxAreaOfIsland/MaxAreaOfIsland.java
+++ src/main/java/challenge/leetcode/medium/maxAreaOfIsland/MaxAreaOfIsland.java
@@ -28,48 +28,6 @@ import java.util.Map;
  * @author Administrator
  * @Created 19/09/2025, 07:57 Fri 19 September 2025
  * @Project 3C: Competitive Coding Challenges
- * @Problem
- * @Name
- * @Difficulty
- * @Tags
- * @link <a href="">www.leetcode.com/999999999999999999999</a>
- * @Description Description
- *
- * <ul>
- * 		<li><b>Example 1:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 				<br>&emsp;
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 2:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 3:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- *
- * <p>
- * <b>Constraints:</b>
- * <ul>
- * 		<li></li>
- * </ul>
- * </p>
- *
- * <br>
- * <hr>
- * @Similar <a href=""/>1000. NAME (Leetcode)</a><br>
- * <br>
- * {@link solution. 10000. NAME (local)}<br>
  **/
 public class MaxAreaOfIsland
 {
@@ -138,7 +96,6 @@ public class MaxAreaOfIsland
 			dfs(i, j + 1, matrix);
 	}
 
-
 	public static int maxAreaOfIslandWithMarkingIslands(int[][] coord)
 	{
 		if (coord.length == 0 || coord[0].length == 0)
@@ -203,9 +160,8 @@ public class MaxAreaOfIsland
 		dfs(coord, x, y - 1, xSize, ySize, islandSize, id);
 	}
 
-
 	// -----------------------------------------------------------------------------------
-	//
+	// Testing algorithms
 	// -----------------------------------------------------------------------------------
 
 	// Public entry point with suffix-ones enabled by default
