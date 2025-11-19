3fec29ad1a1866f77fe147200fd3fe6df694a514
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 15:58:22 +0100

    refactor: mv unofficial to personal dir

--- src/main/java/challenge/personal/NumberOfIslands.java
+++ src/main/java/challenge/personal/NumberOfIslands.java
@@ -15,7 +15,7 @@
  *     You should have received a copy of the GNU Affero General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package challenge.leetcode.medium;
+package challenge.personal;
 
 /*
 	Given an m x n 2D binary grid which represents
