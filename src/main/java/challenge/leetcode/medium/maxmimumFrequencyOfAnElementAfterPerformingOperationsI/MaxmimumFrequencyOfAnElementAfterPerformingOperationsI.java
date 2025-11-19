2b53e69f3e18bacead8451936deac9bbddc73379
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 15:29:19 +0100

    solutions: cleanup lc 3346

--- src/main/java/challenge/leetcode/medium/maxmimumFrequencyOfAnElementAfterPerformingOperationsI/MaxmimumFrequencyOfAnElementAfterPerformingOperationsI.java
+++ src/main/java/challenge/leetcode/medium/maxmimumFrequencyOfAnElementAfterPerformingOperationsI/MaxmimumFrequencyOfAnElementAfterPerformingOperationsI.java
@@ -22,55 +22,7 @@ package challenge.leetcode.medium.maxmimumFrequencyOfAnElementAfterPerformingOpe
  * @author Administrator
  * @Created 21/10/2025, 23:40 Tue 21 October 2025
  * @Project 3C: Competitive Coding Challenges
- * @Problem
- * @Name
- * @Difficulty
- * @Tags
- * @link <a href="">www.leetcode.com/999999999999999999999</a>
- * @Description Description
- *
- * <ul>
- * 		<li><b>Example 1:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 				<br>&emsp;
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 2:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 3:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- *
- * <p>
- * <b>Constraints:</b>
- * <ul>
- * 		<li></li>
- * </ul>
- * </p>
- *
- * <br>
- * <hr>
- * @Similar <a href=""/>1000. NAME (Leetcode)</a><br>
- * <br>
- * {@link solution. 10000. NAME (local)}<br>
  **/
 public class MaxmimumFrequencyOfAnElementAfterPerformingOperationsI
 {
-	static void main(String[] args)
-	{
-
-	}
-
-
 }
