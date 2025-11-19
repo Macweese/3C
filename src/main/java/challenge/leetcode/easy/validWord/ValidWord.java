9c6a71fdcc7454b7c7ecdc1a8d33b57287dd68c8
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 12:57:05 +0100

    solutions: cleanup lc 3136

--- src/main/java/challenge/leetcode/easy/validWord/ValidWord.java
+++ src/main/java/challenge/leetcode/easy/validWord/ValidWord.java
@@ -22,56 +22,9 @@ package challenge.leetcode.easy.validWord;
  * @author Administrator
  * @Created 22/09/2025, 20:21 Mon 22 September 2025
  * @Project 3C: Competitive Coding Challenges
- * @Problem
- * @Name
- * @Difficulty
- * @Tags
- * @link <a href="">www.leetcode.com/999999999999999999999</a>
- * @Description Description
- *
- * <ul>
- * 		<li><b>Example 1:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 				<br>&emsp;
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 2:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- * <ul>
- * 		<li><b>Example 3:</b>
- * 			<br><b>Input:</b> <code>INPUT_INPUT_INPUT</code>
- * 			<br><b>Output:</b> {@code OUTPUT_OUTPUT_OUTPUT}
- * 			<br><b>Explanation:</b>
- * 		</li>
- * </ul>
- *
- * <p>
- * <b>Constraints:</b>
- * <ul>
- * 		<li></li>
- * </ul>
- * </p>
- *
- * <br>
- * <hr>
- * @Similar <a href=""/>1000. NAME (Leetcode)</a><br>
- * <br>
- * {@link solution. 10000. NAME (local)}<br>
  **/
 public class ValidWord
 {
-	static void main(String[] args)
-	{
-		System.out.println(Solution.isValid("234Adas"));
-	}
-
 	public static boolean isValid(String s)
 	{
 		if (s.length() < 3)
@@ -104,23 +57,6 @@ public class ValidWord
 		return hasVowel && hasConsonant;
 	}
 
-	private static boolean isVowel(char c)
-	{
-		c = Character.toLowerCase(c);
-		return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-	}
-}
-class Solution
-{
-	public static boolean isValid(String s)
-	{
-		boolean v = false, k = v;
-		for (char c : s.toCharArray()) {
-			if (!Character.isLetterOrDigit(Character.toLowerCase(c))) return false;
-			if (isVowel(c)) v = true; else if (Character.isLetter(c)) k = true;
-		}
-		return s.length() > 2 && v && k;
-	}
 	private static boolean isVowel(char c)
 	{
 		c = Character.toLowerCase(c);
