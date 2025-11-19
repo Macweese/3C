33b55a9ad72049603498c7f499525a04469786e1
Macweese <50101641+Macweese@users.noreply.github.com>
2025-11-19 13:28:56 +0100

    solutions: cleanup lc 1526

--- src/main/java/challenge/leetcode/hard/minimumNumberOfIncrementsOnSubarraysToFormATargetArray/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
+++ src/main/java/challenge/leetcode/hard/minimumNumberOfIncrementsOnSubarraysToFormATargetArray/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
@@ -22,18 +22,9 @@ package challenge.leetcode.hard.minimumNumberOfIncrementsOnSubarraysToFormATarge
  * @author Administrator
  * @Created 30/10/2025, 02:26 Thu 30 October 2025
  * @Project 3C: Competitive Coding Challenges
- * @Problem
- * @Name
- * @Difficulty
- * @Tags
  **/
 public class MinimumNumberOfIncrementsOnSubarraysToFormATargetArray
 {
-	static void main(String[] args)
-	{
-
-	}
-
 	public static int minNumberOperations(int[] ints)
 	{
 		int ops = 0;
