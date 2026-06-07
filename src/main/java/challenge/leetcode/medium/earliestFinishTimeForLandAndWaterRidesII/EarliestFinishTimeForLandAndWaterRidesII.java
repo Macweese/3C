c4efd1e30b0a01e8304da1089a9fe9475987cb1f
Macweese <50101641+Macweese@users.noreply.github.com>
2026-06-07 16:20:23 +0200

    solutions: add lc 3635

---
+++ src/main/java/challenge/leetcode/medium/earliestFinishTimeForLandAndWaterRidesII/EarliestFinishTimeForLandAndWaterRidesII.java
@@ -0,0 +1,32 @@
+/*
+ *
+ *     Copyright (C) 2026 Macweese <https://www.github.com/Macweese>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package challenge.leetcode.medium.earliestFinishTimeForLandAndWaterRidesII;
+
+/**
+ * @author Administrator
+ * @Created 06:04 Wed 03 June 2026
+ * @Project 3C: Competitive Coding Challenges
+ **/
+public class EarliestFinishTimeForLandAndWaterRidesII
+{
+	public static int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration)
+	{
+		return 0;
+	}
+}
